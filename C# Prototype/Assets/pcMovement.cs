45d40326fdfef9e8caa56231e9eb9daeec7a1dd7
ZeevSheli <sar.noren@gmail.com>
2022-09-27 22:00:29 +0200

    bABY

---
+++ C# Prototype/Assets/pcMovement.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pcMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //velocity = 0
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // if input is pressed, move character
+
+
+        // if on the ground, using raycast, the player can jump. Tag could be used.
+    }
+}
